0fad847cd323b10a539da586dbebfb94ed6bafc8
djdario14 <djdario14doxis@gmail.com>
2026-01-25 01:09:04 -0500

    fix: eliminar ruta y referencia a DetalleClientePage (archivo borrado)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ClientesPage from '../pages/ClientesPage';
-import DetalleClientePage from '../pages/DetalleClientePage';
 import RegistrarGastoPage from '../pages/RegistrarGastoPage';
 import ReporteDiarioPage from '../pages/ReporteDiarioPage';
 
@@ -9,7 +8,6 @@ const AppRoutes: React.FC = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<ClientesPage />} />
-      <Route path="/clientes/:id" element={<DetalleClientePage />} />
       <Route path="/registrar-gasto" element={<RegistrarGastoPage />} />
       <Route path="/reportes" element={<ReporteDiarioPage />} />
     </Routes>
